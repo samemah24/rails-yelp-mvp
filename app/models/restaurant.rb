367295f548971768d44dedafbe8f460ec5ee9d75
Same Mahmoud <samemahmoud24@gmail.com>
2022-05-18 14:24:44 +0200

    Validations done for review and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :phone_number, presence: true
+  CATEGORIES = %w[chinese italian japanese french belgian]
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :name, presence: true
-  validates :address, presence: true
-  validates :category, presence: true
+  validates :content, presence:true
+  validates :rating, presence: true ,inclusion: { in: 0..5 },numericality: { only_integer: true }
 end
